ea1ae274568aaba95017893074e9774a61379752
ssheaks <sarah.sheaks@gmail.com>
2018-05-25 08:49:13 -0700

    add delete functionality

--- index.js
+++ index.js
@@ -11,15 +11,87 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
+const cats = [{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+},
+{
+  imageURL:'https://pet-uploads.adoptapet.com/1/4/b/59371175.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Leopold',
+  sex: 'Male',
+  age: 8,
+  breed: 'Maine Coon',
+  story: 'Owner could no longer care for him'
+},
+{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Bob',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+},
+];
+
+const dogs = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+},
+{
+  imageURL: 'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/German-Shepherd-Dog-2.jpg',
+  imageDescription: 'An energetic pup looking for love and cuddles.',
+  name: 'Gazelle',
+  sex: 'Female',
+  age: 2,
+  breed: 'German Shepherd',
+  story: 'Needed more space and attention'
+},
+{
+  imageURL: 'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Siberian-Husky-2.jpg',
+  imageDescription: 'A loving girl looking fo her forever home.',
+  name: 'Maggie',
+  sex: 'Female',
+  age: 7,
+  breed: 'Siberian husky',
+  story: 'Found abandoned'
+}
+];
+
 app.get('/api/cat', (req, res) => {
   //show the cat that is next in line to be adopted
-  return res.json(//return a cat
-  );});
+  return res.json(cats[0]);
+});
 
 app.get('/api/dog', (req, res) => {
   //Show the dog that is next in line to be adopted
-  return res.json(//return a dog
-  );});
+  return res.json(dogs[0]);
+});
+
+app.delete('/api/cat', (req, res) => {
+  cats.splice(0,1);
+  res.json(cats[0]);
+  console.log('cat adopted!');
+  res.status(204);
+});
+
+app.delete('/api/dog', (req, res) => {
+  dogs.splice(0,1);
+  res.json(dogs[0]);
+  console.log('dog adopted!');
+  res.status(204);
+});
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -33,7 +105,7 @@ app.use(
   })
 );
 
-app.use('/api', petRouter);
+// app.use('/api', petRouter);
 
 function runServer(port = PORT) {
   const server = app
